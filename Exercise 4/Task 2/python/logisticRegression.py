c60db6fe3f71589f7fa45673c5da66a13456f7b0
Christos Trimas <christos.trimas@gmail.com>
2020-06-09 11:20:27 +0300

    Finished the ANN
    accuracy almost 90%.
    Forward: Sigmoid.
    Backward: Gradient Descent.
    Cost: Cross Entropy.

--- Exercise 4/Task 2/python/logisticRegression.py
+++ Exercise 4/Task 2/python/logisticRegression.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from pylab import *
 
 # Load data
 fid = open('exam_scores_data1.txt', 'r')
@@ -21,16 +22,6 @@ X_all = np.array(input_list, dtype=np.float32)
 # Vector of target values. shape = (num_examples, )
 Y_all = np.array(target_list, dtype=np.float32)
 
-ones = np.argwhere(Y_all==1)
-zeros = np.argwhere(Y_all==0)
-
-plt.figure()
-plt.scatter(X_all[ones,0],X_all[ones,1])
-plt.scatter(X_all[zeros,0],X_all[zeros,1])
-plt.xlabel("Grade 1")
-plt.ylabel("Grade 2")
-plt.show()
-
 N = X_all.shape[0]  # Number of examples
 
 # Normalize input data
@@ -39,21 +30,16 @@ m = np.mean(X_all)
 s = np.std(X_all)
 
 X_all = (X_all-m)/s
-# print(X_all) #works fine
+
 # Define the model
 num_features = 2
 output_depth = 1
 batch_size = 8
 learning_rate = 0.001
 
-
+# sigmoid activation function
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
-
-def sigmoid_derivative(z):
-	sig = sigmoid(z)
-	return sig*(1-sig)
-
 
 class NeuralNetwork:
     def __init__(self, input_depth, output_depth, learning_rate):
@@ -62,63 +48,51 @@ class NeuralNetwork:
         self.b = np.zeros((output_depth, ), dtype=np.float32)
         self.learning_rate = learning_rate
 
+    # forward propagation using sigmoid fuction
     def forward(self, x):
-        tmp = np.dot(x,self.W) + self.b
+        tmp = np.mat(x) * np.mat(self.W) + self.b
         output = sigmoid(tmp) #calling activation function
         return output
 
+    # backward propagation using gradient descent 
     def backward(self, X, Y, Y_predicted):
+
         # your code here
-
         m = X.shape[0]
+
         dZ = Y_predicted - Y
+
         dW = 1/m * np.dot(dZ.T,X)
-        db = 1/m * np.sum(dZ,axis=1,keepdims=True)
-        # Y_predicted_delta = self.loss * sigmoid_derivative(Y_predicted)
-
-        # h_layer_error = Y_predicted_delta.dot(self.W.T) #input to hidden 
-        # h_layer_delta_W = h_layer_error * sigmoid_derivative(Y_predicted)
-        
-        # d_CE_d_W = [X.T.dot(h_layer_delta_W), Y_predicted.T.dot(Y_predicted_delta)]
-
-        # d_CE_d_b = Y_predicted_delta
-
-        # inputs = X.T
-
-        # d_CE_d_W = self.learning_rate * np.dot(inputs, Y_predicted_delta)
-
-        # d_CE_d_b = Y_predicted_delta
+        db = 1/m * np.sum(dZ)
 
         return dW , db
 
-        # m = X.shape[0]
-
-        # dA = -(np.divide(Y,Y_predicted) - np.divide(1-Y,1-Y_predicted))
-        # dZ = sigmoid_derivative(dA,X)
-
-        # dW = np.dot(dZ,Y_predicted.T)*(1/m)
-        # dB = np.sum(dZ)/m
-
-        # return dW, dB
-
+    # cost calculation using cross entropy
     def cross_entropy(self, Y, Y_predicted):
         # your code here
         self.loss = -np.mean(np.multiply(Y, np.log(Y_predicted)) + np.multiply((1-Y), np.log(1-Y_predicted)))
-        # print("Loss=",loss)
+
         return self.loss
 
+    # update parameters
     def update_weights(self, d_CE_d_W, d_CE_d_b):
-        self.W = self.W - self.learning_rate * d_CE_d_W
+        tmp = np.dot(self.learning_rate,d_CE_d_W)
+        tmp = tmp.reshape(2,1)
+        self.W = self.W - tmp
         self.b = self.b - self.learning_rate * d_CE_d_b
-
-        # self.W = self.W - d_CE_d_W
-        # for i in d_CE_d_b:
-        # 	self.b = self.b - self.learning_rate * d_CE_d_b
+    # make prediction
+    def predict(self, x):
+    	prediction = nn.forward(x)
+    	for i in range(np.size(prediction)):
+        	if prediction[i] > 0.5:
+        		prediction[i] = int(1)
+        	else:
+        		prediction[i] = int(0)
+    	return prediction
 
 
 
 nn = NeuralNetwork(num_features, output_depth, learning_rate)
-# print("Weight0=",nn.W)
 num_epochs = 55
 num_batches = N - batch_size + 1
 
@@ -137,17 +111,51 @@ for epoch in range(num_epochs):
     epoch_loss /= num_batches
     print('epoch_loss = ', epoch_loss)
 
-# print('wieghts=',nn.W)
-
 # Using the trained model to predict the probabilities of some examples and the compute the accuracy
 
 # Predict the normalized example [45, 85]
-# example = (np.array([[45, 85]], dtype=np.float32) - m)/s
+example = (np.array([[45, 85]], dtype=np.float32) - m)/s
+print('\nPredicting the probabilities of example [45, 85]\n')
 
-# print('Predicting the probabilities of example [45, 85]')
-# your code here
+Pr_example_2 = nn.forward(example)
+print("Probability = ",Pr_example_2)
+print("\n")
 
 # Predict the accuracy of the training examples
-# accuracy_np = .... # your code here
+Y_predicted = nn.predict(X_all)
 
-# print('accuracy = ', accuracy_np)
+correct = 0
+for t in range(Y_all.shape[0]):
+	if Y_all[t] == Y_predicted[t]:
+		correct += 1
+
+accuracy_np = correct / np.size(Y_all)
+
+print('accuracy = ', accuracy_np)
+
+
+print("Do you want to print the dataset?\n")
+image = input("Press 1 for yes, or anything for no:")
+
+if image == '1':
+	ones = np.argwhere(Y_all==1)
+	zeros = np.argwhere(Y_all==0)
+	plt.figure()
+	plt.subplot(2,2,1)
+	xticks([]), yticks([])
+	plt.title("Original dataset")
+	plt.scatter(X_all[ones,0],X_all[ones,1])
+	plt.scatter(X_all[zeros,0],X_all[zeros,1])
+	plt.xlabel("Grade 1")
+	plt.ylabel("Grade 2")
+
+	plt.subplot(2,2,2)
+	xticks([]), yticks([])
+	ones = np.argwhere(Y_predicted==1)
+	zeros = np.argwhere(Y_predicted==0)
+	plt.title("NeuralNetwork prediction")
+	plt.scatter(X_all[ones,0],X_all[ones,1])
+	plt.scatter(X_all[zeros,0],X_all[zeros,1])
+	plt.xlabel("Grade 1")
+	plt.ylabel("Grade 2")
+	plt.show()
